f9f62ae19a4d92f06c2b40ed26323f7b9e2a1841
Dnutz <nuttall_5@hotmail.com>
2017-12-03 21:50:59 -0800

    final fixes for homework 4

--- vanilla-src/CRUD_roster.js
+++ vanilla-src/CRUD_roster.js
@@ -2,10 +2,10 @@ function initializeRoster(amount) {
     //alert('initializing roster');
     var players = [];
 
-    if (amount == 0) {
+    /*if (amount == 0) {
         alert('Please specify the number of players you want to create');
         return 0;
-    }
+    }*/
 
     for (var i = 0; i < amount; i++) {
 
@@ -178,41 +178,44 @@ function initializeInputStats() {
     var inputRoster = JSON.parse(localStorage.getItem('Roster'));
     //console.log(inputRoster);
     for (var i = 0; i < inputRoster.length; ++i){
-        var name = inputRoster[i].name;
-        var number = inputRoster[i].number;
-        var position = inputRoster[i].position;
-        var template = document.getElementById('playerTemplate').content;
-        //console.log(template);
-        //template.querySelector('#tempName').innerText = name;
-        //console.log(template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[1]);
-        template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[1].innerText = name;
-        //template.firstChild.firstChild.firstChild.firstChild.childNodes[1].firstChild.innerText = name;
-        //template.querySelector('#tempPos').innerText = position;
-        template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[3].innerText = position;
-        //template.querySelector('#tempNum').innerText = number;
-        template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].innerText = number;
-        //console.log(template.querySelector('#playerDiv'));
-        //var f = ('show_modal('+name+')');
-        //template.querySelector('#playerDiv').setAttribute('onClick',f);
-        //template.querySelector('#playerDiv').setAttribute('onClick','show_modal('+name+')');
-        //template.querySelector('#playerDiv').setAttribute('onClick','show_modal("'+name+'")');
-        template.childNodes[1].childNodes[1].setAttribute('onClick','show_modal("'+name+'")');
-        //console.log(template.childNodes[1].childNodes[1]);
-        //template.querySelector('#playerDiv').setAttribute('onclick',function f(){show_modal(name)});
-        //template.querySelector('#playerDiv').onclick = function f(){show_modal(name)};
-        //console.log(template.querySelector('#playerDiv'));
-        var toAdd = document.getElementById('playerTemplate').content.cloneNode(true);
-        //console.log(toAdd);
-        document.getElementById('IStbody').appendChild(toAdd);
+        if (inputRoster[i].inactive == false) {
+            var id = inputRoster[i].ID;
+            var name = inputRoster[i].name;
+            var number = inputRoster[i].number;
+            var position = inputRoster[i].position;
+            var template = document.getElementById('playerTemplate').content;
+            //console.log(template);
+            //template.querySelector('#tempName').innerText = name;
+            //console.log(template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[1]);
+            template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[1].innerText = name;
+            //template.firstChild.firstChild.firstChild.firstChild.childNodes[1].firstChild.innerText = name;
+            //template.querySelector('#tempPos').innerText = position;
+            template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].childNodes[3].innerText = position;
+            //template.querySelector('#tempNum').innerText = number;
+            template.childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[1].childNodes[3].innerText = number;
+            //console.log(template.querySelector('#playerDiv'));
+            //var f = ('show_modal('+name+')');
+            //template.querySelector('#playerDiv').setAttribute('onClick',f);
+            //template.querySelector('#playerDiv').setAttribute('onClick','show_modal('+name+')');
+            //template.querySelector('#playerDiv').setAttribute('onClick','show_modal("'+name+'")');
+            template.childNodes[1].childNodes[1].setAttribute('onClick','show_modal("'+id+'")');
+            //console.log(template.childNodes[1].childNodes[1]);
+            //template.querySelector('#playerDiv').setAttribute('onclick',function f(){show_modal(name)});
+            //template.querySelector('#playerDiv').onclick = function f(){show_modal(name)};
+            //console.log(template.querySelector('#playerDiv'));
+            var toAdd = document.getElementById('playerTemplate').content.cloneNode(true);
+            //console.log(toAdd);
+            document.getElementById('IStbody').appendChild(toAdd);
+        }
     }
     return 1;
 }
 
-function viewStats(name){
+function viewStats(id){
     var inputRoster = JSON.parse(localStorage.getItem('Roster'));
     for (var i = 0; i < inputRoster.length; ++i){
         var player = inputRoster[i];
-        if (player.name == name) {
+        if (player.ID == id) {
             document.getElementById('fouls').innerText = player.stats.fouls;
             document.getElementById('redCards').innerText = player.stats.redCards;
             document.getElementById('yellowCards').innerText = player.stats.yellowCards;
@@ -222,17 +225,17 @@ function viewStats(name){
             document.getElementById('pKicks').innerText = player.stats.pKicks;
             document.getElementById('tIns').innerText = player.stats.tIns;
             document.getElementById('appearances').innerText = player.stats.appearances;
-            document.getElementById('update_btn').setAttribute('onClick','updateStats("'+name+'")');
+            document.getElementById('update_btn').setAttribute('onClick','updateStats("'+id+'")');
             break;
         }
     }
 }
 
-function updateStats(name){
+function updateStats(id){
     var inputRoster = JSON.parse(localStorage.getItem('Roster'));
     for (var i = 0; i < inputRoster.length; ++i){
         var player = inputRoster[i];
-        if (inputRoster[i].name == name) {
+        if (inputRoster[i].ID == id) {
             player.stats.fouls = document.getElementById('fouls').innerText;
             player.stats.redCards = document.getElementById('redCards').innerText;
             player.stats.yellowCards = document.getElementById('yellowCards').innerText;
--- vanilla-src/hideInputTab.js
+++ vanilla-src/hideInputTab.js
@@ -1,9 +1,9 @@
 function hideInputTab() {
     //check user type
     if (localStorage.getItem('currentUser') != undefined) {
-        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        var userType = currentUser['type'];
-        if (userType != 'coach'){
+        var currUser = JSON.parse(localStorage.getItem('currentUser'));
+        var userType = currUser['type'];
+        if (userType != 'Coach'){
             document.getElementsByClassName('inputTab')[0].style.display = 'none';
         }
     }
